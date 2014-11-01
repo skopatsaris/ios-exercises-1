d268b84bf4fea7bfff765ece00d2318fbca6d25b
Sotiris Kopatsaris <skopatsaris@gmail.com>
2014-11-01 17:03:13 +0100

    Many errors, checkpoint Simple Data Types and Objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -11,8 +11,12 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+    //WHAT I HAVE TO DO IS TO BASICALLY UNBOX THE NSNUMBER TO AN INT, DOUBLE IT AND THEN TURN IT INTO AN NSNUMBER AGAIN AND RETURN IT?
+    NSInteger *Numbertobedoubled = [number intValue];
+    NSInteger *DoubleNumber =   Numbertobedoubled * 2;
+    NSNumber *bigNumber = [NSNumber numberWithInt:DoubleNumber];
+    //DUNNO WHY I GET SO MANY ERRORS
+    return bigNumber;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
